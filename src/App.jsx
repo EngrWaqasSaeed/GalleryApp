121eb0a60784c4231c837e91704caa1c90943bb7
EngrWaqasSaeed <waqassaeed633@gmail.com>
2026-07-19 23:35:52 +0500

    Api inetegrate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-
+import axios from 'axios'
 const App = () => {
+  const [userData, setUserData] = useState([])
+  const getData = () => {
+    const response = axios.get('https://picsum.photos/v2/list?page=2&limit=100')
+    setUserData(response.data)
+  }
   return <div className='bg-amber-500'>App</div>
 }
 
