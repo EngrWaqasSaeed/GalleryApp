92d00d3e291c41e53f80738817ed41164d33fcc4
EngrWaqasSaeed <waqassaeed633@gmail.com>
2026-07-19 23:42:34 +0500

    frontend Completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,21 @@ const App = () => {
     const response = axios.get('https://picsum.photos/v2/list?page=2&limit=100')
     setUserData(response.data)
   }
-  return <div className='bg-amber-500'>App</div>
+  let printUserData = 'No user Available'
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {})
+  }
+  return (
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
+      <button
+        onClick={getData}
+        className='bg-green-600 active:scale-95 mb-3 px-5 py-5 rounded text-white'
+      >
+        Get Data
+      </button>
+      <div className='flex flex-wrap gap-4'>{printUserData}</div>
+    </div>
+  )
 }
 
 export default App
